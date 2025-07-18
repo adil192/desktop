30f89a51f92bcf11a456bcb5c37eeaef0642150b
Adil Hanney <adilhanney@disroot.org>
2025-07-18 18:15:26 +0100

    Fix linux compilation issues

--- app/src/main-process/main.ts
+++ app/src/main-process/main.ts
@@ -289,7 +289,7 @@ async function handleCommandLineArguments(argv: string[]) {
     // so we should filter out the program name as well as any parameters that
     // look like arguments to Electron
     const argsWithoutParameters = args.filter(
-      a => !a.endsWith('github-desktop') && !a.startsWith('--')
+      (a: string) => !a.endsWith('github-desktop') && !a.startsWith('--')
     )
     if (argsWithoutParameters.length > 0) {
       handleAppURL(argsWithoutParameters[0])
